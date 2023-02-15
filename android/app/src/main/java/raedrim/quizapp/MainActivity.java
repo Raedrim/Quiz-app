7487ba68b4a82634101a94e209b0bfdf3a68cc43
Raedrim <43709947+Raedrim@users.noreply.github.com>
2023-02-15 11:51:52 +0100

    Added capacitor

---
+++ android/app/src/main/java/raedrim/quizapp/MainActivity.java
@@ -0,0 +1,5 @@
+package raedrim.quizapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'raedrim.quizapp',
+  appName: 'quiz-app',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
